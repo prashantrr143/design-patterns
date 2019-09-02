60ed92d82e70453f0e11598320405caea6321731
Prashant Kumar Singh <prashant.sirt153@gmail.com>
2019-09-02 11:58:09 +0530

    Initial Commit

---
+++ singleton/src/main/java/io/prashant/learing/dp/singleton/App.java
@@ -0,0 +1,13 @@
+package io.prashant.learing.dp.singleton;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
