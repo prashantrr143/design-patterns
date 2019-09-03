fc819bef7a61dc4fb1875088d0b1594183cbe955
Prashant Kumar Singh <prashant.sirt153@gmail.com>
2019-09-03 16:09:35 +0530

    Init Command Design Pattern setup

---
+++ command/src/main/java/io/prashant/learning/dp/command/App.java
@@ -0,0 +1,13 @@
+package io.prashant.learning.dp.command;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
